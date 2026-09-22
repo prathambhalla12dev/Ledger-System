a5509e2e7012afe5f679f3a20263b3f4e31abb43
Pratham <astrotalk@Astrotalks-MacBook-Pro-7.local>
2026-09-22 20:57:48 +0530

    bootstrap Spring Boot project with Oracle and Kafka compose

---
+++ Ledger_System/src/main/java/dev/pratham/banking_ledger/BankingLedgerApplication.java
@@ -0,0 +1,17 @@
+package dev.pratham.banking_ledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableScheduling
+public class BankingLedgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingLedgerApplication.class, args);
+    }
+
+}
